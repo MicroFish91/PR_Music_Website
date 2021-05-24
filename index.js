4af21a7414ec72ceeb145616736fa974cd38bb9d
Matthew Fisher <mwfisher91@gmail.com>
2021-05-24 16:15:07 -0700

    First Commit: Template Finished for music website

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set Static Dir.
+app.use(express.static('public'));
+
+// Set View Engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Routes
+app.use(require('./routes/albums'));
+app.use(require('./routes/'));
+
+app.listen(port, () => console.log(`Listening on port ${port}.`))
