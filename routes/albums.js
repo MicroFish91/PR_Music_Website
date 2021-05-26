481c0d311e666f60ab5c0290382a9d474bbb1c25
Matthew Fisher <mwfisher91@gmail.com>
2021-05-25 17:00:55 -0700

    Finished both album routes + css

--- routes/albums.js
+++ routes/albums.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const albums = require('../data/data.json').albums;
 
 router.get('/albums', (req, res) => {
-  res.render('albums');
+  res.render('albums', {
+    album: null,
+    albums: albums,
+    pageId: 'albums',
+    pageTitle: "Discography"
+  });
+})
+
+router.get('/albums/:id', (req, res) => {
+  const { id } = req.params;
+  res.render('albums', {
+    album: albums[id],
+    albums: null,
+    pageId: 'albums',
+    pageTitle: albums[id].shortname
+  });
 })
 
 module.exports = router;
